44def59e343685d44a0031f0b1e47a76a56176c4
notakm <rsakshad@gmail.com>
2026-08-05 13:14:17 +0530

    added layout for credit card, fonts, top section covered partially

--- lib/DesktopHome.dart
+++ lib/DesktopHome.dart
@@ -7,37 +7,60 @@ class DesktopHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      body: Center(
-        child: LayoutBuilder(builder: ((context, constraints) {
-         return Container( decoration: BoxDecoration(
-            border: BoxBorder.all(
-              width: 2
-            ),
-            color: Colors.black,
-            
-          ),
-          width: constraints.maxWidth * 0.8,
-          height: constraints.maxHeight * 0.8,
-          child: LayoutBuilder( builder: (context, constraints) {
-            return Stack(
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      right: 3
-                    ),
-                    child: Text("Credit Card ${(constraints.maxWidth * 0.03).clamp(21,35)}" , style: TextStyle(
-                      color: Colors.white,
-                      fontSize: (constraints.maxWidth * 0.03).clamp(21,35)
-                    ),),
+      body:Center(
+        child: LayoutBuilder(builder: (context , constraints){
+          return Container(
+            height: constraints.maxHeight * 0.8,
+            width: constraints.maxWidth * 0.8,
+            color: Colors.blue,
+            child: LayoutBuilder(builder: (context, constraints) {
+              return Column(
+                children: [  
+                  // Top "Credit Card + Logo" Section
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: constraints.maxWidth * 0.99,
+                    color: Colors.red,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                          color: Colors.orange,
+                          child: Text("Credit Card\nMockup", style: TextStyle(
+                            fontFamily: "Montserrat",
+                            fontSize: constraints.maxHeight * 0.1
+                          ),),  
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                          color: Colors.yellowAccent,
+                          child: Text("hi"),  
+                          ),
+                        ),
+                        // Text('Hi')
+                      ],
+                    )
                   ),
-                )
-              ],
-            );
-        }),);
-        })),
-      ),
+                                  Expanded(
+                    child: Container(
+                      color: Colors.green
+                    )
+              
+                  ),
+                                  Expanded(
+                    child: Container(
+                      color: Colors.yellow
+                    )
+              
+                  ),
+              
+                ],
+              );
+        })
+          );
+        }),
+      )
     );
   }
 }
